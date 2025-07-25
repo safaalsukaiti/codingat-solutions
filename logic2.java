c8b0783a2cf7e14c39ba71871db446ce851f2836
Safa Alsukaiti <alsukaitisafa@gmail.com>
2025-07-25 12:19:48 +0400

    solving logical problems

---
+++ logic2.java
@@ -0,0 +1,11 @@
+package logic1;
+
+public class logic2 {
+    int teenSum(int a, int b) {
+        if ((a >= 13 && a <= 19) || (b >= 13 && b <= 19)) {
+            return 19;
+        }
+        return a + b;
+    }
+
+}
